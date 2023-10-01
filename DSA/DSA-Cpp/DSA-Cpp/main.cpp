70ffa74e7ccb92c6f59a9a234bef5932bdcd7b4f
Muhammad Muzamal <muzamal@codesorbit.com>
2023-10-01 18:32:05 +0500

    add dsa folder

---
+++ DSA/DSA-Cpp/DSA-Cpp/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  DSA-Cpp
+//
+//  Created by Codes Orbit on 30/09/2023.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    vector<int> v;
+    v.push_back(100);
+    cout<<v[0];
+    cout << "Hello, World!\n";
+    print()
+    return 0;
+}
